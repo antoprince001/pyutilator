ea9f1cf38d202b86964c4aa97d2a9e22d0a24708
antoprince001 <antoprince001@gmail.com>
2023-02-07 22:07:24 +0530

    pyutilator version v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "pyutilator",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Antony Prince J",
     author_email = "antoprince001@gmail.com",
     description = "Python Utility Decorators",
